c813cc445a0a5874e40077a82eb3a933b59b2cb2
rzjrh <rzjrh@hotmail.com>
2019-09-29 11:31:34 -0500

    first commit

---
+++ src/main/java/com/RezaAk/web/ControllerAndView/controllers/Users.java
@@ -0,0 +1,70 @@
+package com.RezaAk.web.ControllerAndView.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.RezaAk.web.ControllerAndView.models.User;
+import com.RezaAk.web.ControllerAndView.services.UserService;
+
+@Controller
+public class Users {
+    private final UserService userService;
+    
+    public Users(UserService userService) {
+        this.userService = userService;
+    }
+    
+    @RequestMapping("/registration")
+    public String registerForm(@ModelAttribute("user") User user) {
+        return "registrationPage.jsp";
+    }
+    @RequestMapping("/login")
+    public String login() {
+        return "loginPage.jsp";
+    }
+    
+    @RequestMapping(value="/registration", method=RequestMethod.POST)
+    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
+        if(result.hasErrors()) {
+        	return "registrationPage.jsp";
+        }
+        User u = userService.registerUser(user);
+        session.setAttribute("userId", u.getId());
+        return "redirect:/home";
+    }
+    
+    @RequestMapping(value="/login", method=RequestMethod.POST)
+    public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
+        boolean isAuthenticated = userService.authenticateUser(email, password);
+        if(isAuthenticated) {
+        	User u = userService.findByEmail(email);
+        	session.setAttribute("userId", u.getId());
+        	return "redirect:/home";
+        }
+        else {
+        	model.addAttribute("error", "Invalid Credential. Please try again.");
+        	return "loginPage";
+        }
+    }
+    
+    @RequestMapping("/home")
+    public String home(HttpSession session, Model model) {
+    	Long userId = (Long) session.getAttribute("userId");
+    	User u = userService.findUserById(userId);
+    	model.addAttribute("user",u);
+    	return "homePage.jsp";
+    }
+    @RequestMapping("/logout")
+    public String logout(HttpSession session) {
+    	session.invalidate();
+        return "redirect:/login";
+    }
+}
